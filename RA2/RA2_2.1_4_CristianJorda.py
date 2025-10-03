1067fc8010c4c5b35b9b878c30edaf28c9178df1
cJord4 <cjorda@iesjulioantonio.cat>
2025-10-03 08:42:06 +0200

    Update RA2_2.1_4_CristianJorda.py

--- RA2/RA2_2.1_4_CristianJorda.py
+++ RA2/RA2_2.1_4_CristianJorda.py
@@ -1,33 +1,12 @@
-import tkinter as tk
-import time
-import threading
+# Autor: Cristian Jordà
+# Data: 20/09/2023
+# versió: 1.0
+#
+# Descripció: Programa que demana una nota a l'usuari i indica si està aprovat o suspès.
+# Especificacions de entrada: La nota de l'usuari (0-10).
 
-def explotar(root, label):
-    # Esperar 2 segundos antes de "explotar"
-    time.sleep(2)
-    
-    # Simular parpadeo tipo explosión
-    for i in range(6):
-        color = "red" if i % 2 == 0 else "yellow"
-        label.config(bg=color, fg="black")
-        root.update()
-        time.sleep(0.2)
-    
-    # Cerrar ventana
-    root.destroy()
-
-def main():
-    root = tk.Tk()
-    root.title("Explosión")
-    root.geometry("300x200")
-
-    label = tk.Label(root, text="Niggers", font=("Arial", 30))
-    label.pack(expand=True)
-
-    # Lanzamos la explosión en un hilo aparte
-    threading.Thread(target=explotar, args=(root, label), daemon=True).start()
-
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
+nota = int(input("Introdueix la nota: "))
+if nota >= 5:
+    print("Aprovat")
+else:
+    print("Suspès")
