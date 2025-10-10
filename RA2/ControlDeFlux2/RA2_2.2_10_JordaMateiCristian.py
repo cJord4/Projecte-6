c10a380c16b25707f5567786cd0acd0d2e2bb330
cJord4 <cjorda@iesjulioantonio.cat>
2025-10-10 13:04:08 +0200

    Create RA2_2.2_10_JordaMateiCristian.py

---
+++ RA2/ControlDeFlux2/RA2_2.2_10_JordaMateiCristian.py
@@ -0,0 +1,9 @@
+# Autor: Cristian Jorda Matei
+# Data: 08/10/2025
+# Descripció: Utilitzar un bucle per imprimir el següent patró d’estrelles
+
+
+files = 6  
+
+for i in range(1, files + 1):
+    print('*' * i)
