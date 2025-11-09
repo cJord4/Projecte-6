70f804a24b4458f42aa86bf3d60063c4ffa5d728
Cristian Jorda <cjorda@iesjulioantonio.cat>
2025-11-09 19:31:48 +0100

    Add files via upload

---
+++ RA2/2.5/utilitats/moneda.py
@@ -0,0 +1,11 @@
+# Autor: Cristian Jorda Matei
+# Data: 8/11/2025
+# Mòdul per convertir entre euros i dòlars.
+
+def euros_a_dolars(euros):
+    canvi = 1.1
+    return euros * canvi
+
+def dolars_a_euros(dolars):
+    canvi = 1.1
+    return dolars / canvi
---
+++ RA2/2.5/utilitats/temps.py
@@ -0,0 +1,15 @@
+# Autor: Cristian Jorda Matei
+# Data: 8/11/2025
+# Mòdul per convertir segons a hores, minuts, etc.
+
+def segons_a_minuts(segons):
+    return segons / 60
+
+def segons_a_hores(segons):
+    return segons / 3600
+
+def minuts_a_segons(minuts):
+    return minuts * 60
+
+def hores_a_segons(hores):
+    return hores * 3600
