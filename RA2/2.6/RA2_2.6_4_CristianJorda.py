8e6309cc8b2a096d953330d13d6693e01952f2d3
Cristian Jorda <cjorda@iesjulioantonio.cat>
2025-11-19 10:58:58 +0100

    Update RA2_2.6_4_CristianJorda.py

--- RA2/2.6/RA2_2.6_4_CristianJorda.py
+++ RA2/2.6/RA2_2.6_4_CristianJorda.py
@@ -2,9 +2,7 @@
 # Data: 17/11/2025
 # Comptar línies: Llegeix un fitxer i mostra quantes línies té.
 
-print("EXERCICI 1: Comptar línies")
-print("-" * 40)
-
 with open("sortida.txt", "r", encoding="utf-8") as fitxer:
     linies = fitxer.readlines()
+
     print(f"El fitxer té {len(linies)} línies.")
